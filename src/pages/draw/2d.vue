083ed469b925ed5ede89e3b4f5f16f729097e560
chengzhx76 <chengzhx@qq.com>
2021-04-12 22:44:06 +0800

    update

--- src/pages/draw/2d.vue
+++ src/pages/draw/2d.vue
@@ -20,9 +20,10 @@
         src: '',
         posterUrl: 'https://chengzhx76.cn/wechat/shard/50_40.png',
         avatar: 'https://thirdwx.qlogo.cn/mmopen/vi_32/Q3auHgzwzM661zJK006gZLp4FX1V2Eib1nlmcNRvSMGnv00LTDBnG7NslZBZbyom4D3sVnuibsPKpyqhHicOFdNEA/132',
-        qr: 'https://chengzhx76.cn/qrcode/00525818892843b4.png',
+        qrcodeUrl: 'https://chengzhx76.cn/qrcode/00525818892843b4.png',
 
-        avatarDiam: 60, // 头像的直径
+        avatarDiam: 40, // 头像的直径
+        qrcodeDiam: 40, // 小程序码的直径
         avatarMargin: {
           top: 10,
           bottom: 10,
@@ -59,6 +60,8 @@
         this.drawPoster().then(res => {
           // TODO 绘制内容
           this.drawAvatar()
+          this.drawQrcode()
+          this.drawText()
         })
       },
       drawPoster() {
@@ -66,8 +69,8 @@
           const poster = this.canvas.createImage()
           poster.src = this.posterUrl
           poster.onload = () => {
-            this.computeCanvasSize(poster.height, poster.width).then(res => {
-              this.ctx.drawImage(poster, 0, 0, poster.width, poster.height, 0, 0, res.width, res.height)
+            this.computeCanvasSize(poster.height, poster.width).then(() => {
+              this.ctx.drawImage(poster, 0, 0, poster.width, poster.height, 0, 0, this.canvasWidth, this.canvasHeight)
               resolve()
             })
           }
@@ -76,21 +79,13 @@
       },
       computeCanvasSize(imgHeight, imgWidth) {
         return new Promise((resolve, reject) => {
-          // this.posterHeight = this.canvasWidth * (imgHeight / imgWidth)
-          // this.canvasHeight = this.posterHeight
-          // this.posterHeight = imgHeight
-          this.canvasHeight = this.canvasDom.height
-          this.canvasWidth = this.canvasDom.width
-
           this.canvas.width = this.canvasWidth * this.dpr
           this.canvas.height = this.canvasHeight * this.dpr
           this.ctx.scale(this.dpr, this.dpr)
 
           console.log(`computeCanvasSize->证书:${imgHeight} x ${imgWidth}-dpr:${this.dpr}-canvas:${this.canvasHeight} x ${this.canvasWidth}`)
 
-          setTimeout(() => {
-            resolve({ 'width': this.canvasWidth, 'height': this.canvasHeight })
-          }, 1200)
+          resolve()
         })
       },
       drawAvatar() {
@@ -98,14 +93,35 @@
         avatar.src = this.avatar
         avatar.onload = () => {
           const radius = this.avatarDiam / 2
-          const x = 0
-          const y = 0
+          const x = this.avatarMargin.left
+          const y = this.canvasHeight - this.avatarDiam - 10
           this.ctx.save()
           this.ctx.arc(x + radius, y + radius, radius, 0, 2 * Math.PI)
           this.ctx.clip()
           this.ctx.drawImage(avatar, 0, 0, avatar.width, avatar.height, x, y, this.avatarDiam, this.avatarDiam)
           this.ctx.restore()
         }
+      },
+      drawQrcode() {
+        const qrcode = this.canvas.createImage()
+        qrcode.src = this.qrcodeUrl
+        qrcode.onload = () => {
+          const radius = this.qrcodeDiam / 2
+          const x = this.canvasWidth - this.qrcodeDiam - 10
+          const y = this.canvasHeight - this.qrcodeDiam - 10
+          this.ctx.save()
+          this.ctx.arc(x + radius, y + radius, radius, 0, 2 * Math.PI)
+          this.ctx.clip()
+          this.ctx.drawImage(qrcode, 0, 0, qrcode.width, qrcode.height, x, y, this.qrcodeDiam, this.qrcodeDiam)
+          this.ctx.restore()
+        }
+      },
+      drawText() {
+        this.ctx.save()
+        this.ctx.font = '14px Arial'
+        this.ctx.fillStyle = '#333333'
+        this.ctx.fillText('by.Cheng', 10, 20)
+        this.ctx.restore()
       },
 
       save() {
