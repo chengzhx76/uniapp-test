493fb961491cafcdad036746a0c559f1300f563a
chengzhx76 <chengzhx@qq.com>
2021-04-12 12:12:06 +0800

    update

---
+++ src/pages/draw/2d.vue
@@ -0,0 +1,128 @@
+<template>
+  <view class="test-draw">
+    <view class="canvas">
+      <canvas type="2d" id="canvasBox" :style="{height: canvasHeight + 'px', width: canvasWidth + 'px'}"></canvas>
+    </view>
+    <image class="img" :src="src"/>
+    <view class="footer">
+      <button @click="drawImage()">创建证书</button>
+      <button @click="save()">保存证书</button>
+    </view>
+  </view>
+
+</template>
+
+<script>
+  import { loading, hideLoading } from '@/util/wxml'
+  export default {
+    data () {
+      return {
+        src: '',
+        posterUrl: 'https://chengzhx76.cn/wechat/shard/50_40.png',
+        avatar: 'https://thirdwx.qlogo.cn/mmopen/vi_32/Q3auHgzwzM661zJK006gZLp4FX1V2Eib1nlmcNRvSMGnv00LTDBnG7NslZBZbyom4D3sVnuibsPKpyqhHicOFdNEA/132',
+        qr: 'https://chengzhx76.cn/qrcode/00525818892843b4.png',
+
+        avatarDiam: 60, // 头像的直径
+        avatarMargin: {
+          top: 10,
+          bottom: 10,
+          left: 10,
+          right: 10
+        },
+
+        canvasHeight: 200,
+        canvasWidth: 250,
+        posterHeight: 0,
+
+        canvasDom: null,
+        canvas: null,
+        ctx: null,
+        dpr: 1,
+      }
+    },
+    methods: {
+      drawImage() {
+        const query = wx.createSelectorQuery()
+        query.select('#canvasBox')
+              .fields({
+                node: true,
+                size: true
+              }).exec(res => {
+                this.canvasDom = res[0]
+                this.canvas = this.canvasDom.node
+                this.ctx = this.canvas.getContext('2d')
+                this.dpr = wx.getSystemInfoSync().pixelRatio
+                this.drawing()
+              })
+      },
+      drawing() {
+        // loading('生成中')
+        this.drawPoster().then(res => {
+          // TODO 绘制内容
+          this.drawAvatar()
+        })
+      },
+      drawPoster() {
+        return new Promise((resolve, reject) => {
+          const poster = this.canvas.createImage()
+          poster.src = this.posterUrl
+          poster.onload = () => {
+            this.computeCanvasSize(poster.height, poster.width).then(res => {
+              this.ctx.drawImage(poster, 0, 0, poster.width, poster.height, 0, 0, res.width, res.height)
+              resolve()
+            })
+          }
+
+        })
+      },
+      computeCanvasSize(imgHeight, imgWidth) {
+        return new Promise((resolve, reject) => {
+          // this.posterHeight = this.canvasWidth * (imgHeight / imgWidth)
+          // this.canvasHeight = this.posterHeight
+          this.posterHeight = imgHeight
+          this.canvasHeight = this.canvasDom.height
+          this.canvasWidth = this.canvasDom.width
+
+          this.canvas.width = this.canvasWidth * this.dpr
+          this.canvas.height = this.canvasHeight * this.dpr
+          this.ctx.scale(this.dpr, this.dpr)
+
+          console.log(`computeCanvasSize->证书:${imgHeight} x ${imgWidth}-dpr:${this.dpr}-canvas:${this.canvasHeight} x ${this.canvasWidth}`)
+
+          setTimeout(() => {
+            resolve({ 'width': this.canvasWidth, 'height': this.posterHeight })
+          }, 1200)
+        })
+      },
+      drawAvatar() {
+        const avatar = this.canvas.createImage()
+        avatar.src = this.avatar
+        avatar.onload = () => {
+          const radius = this.avatarDiam / 2
+          const x = 0
+          const y = 0
+          this.ctx.save()
+          this.ctx.arc(x + radius, y + radius, radius, 0, 2 * Math.PI)
+          this.ctx.clip()
+          this.ctx.drawImage(avatar, 0, 0, avatar.width, avatar.height, x, y, this.avatarDiam, this.avatarDiam)
+          this.ctx.restore()
+        }
+      },
+
+      save() {
+
+      }
+    }
+  }
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+  @import "@/styles/mixin.scss";
+  @import "@/styles/variables.scss";
+  .img {
+    height: 200px;
+    width: 250px;
+    margin: 10px;
+    @include border($color: #F86D36);
+  }
+</style>
--- src/util/index.js
+++ src/util/index.js
@@ -1,3 +1,47 @@
+// none success
+function toast (title, icon='none', duration=1500) {
+  uni.showToast({
+    title,
+    icon,
+    duration: duration
+  })
+}
+
+function loading (title='加载中') {
+  uni.showLoading({
+    title
+  })
+}
+
+function hideLoading() {
+  uni.hideLoading()
+}
+
+function copy(content, tips='内容已复制') {
+  uni.setClipboardData({
+    data: content,
+    success (res) {
+      toast(tips)
+    }
+  })
+}
+
+function vibrate() {
+  uni.vibrateShort({success: function () {}})
+}
+
+function showModal(content='确定', title='提示') {
+  return new Promise((resolve, reject) => {
+    uni.showModal({
+      title,
+      content,
+      success (res) {
+        resolve(res.confirm)
+      }
+    })
+  })
+}
+
 function rpxToPx(rpx) {
   return rpx / 750 * store.getters.sysInfo.windowWidth
 }
@@ -6,9 +50,16 @@ function pxToRpx(px) {
   return px * 750 / store.getters.sysInfo.windowWidth
 }
 
-
 export {
+  toast,
+  loading,
+  vibrate,
+  hideLoading,
+  showModal,
+
+  copy,
+
   rpxToPx,
-  pxToRpx
+  pxToRpx,
 
 }
