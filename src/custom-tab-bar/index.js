68286314b87401fdeb43263410c33ee2dd35ae36
chengzhx76 <chengzhx76@qq.com>
2023-10-28 11:56:41 +0800

    update

---
+++ src/custom-tab-bar/index.js
@@ -0,0 +1,26 @@
+Component({
+  data: {
+    selected: 0,
+    color: "#7A7E83",
+    selectedColor: "#3cc51f",
+    list: [{
+      pagePath: "/index/index",
+      text: "组件"
+    }, {
+      pagePath: "/index/index2",
+      text: "接口"
+    }]
+  },
+  attached() {
+  },
+  methods: {
+    switchTab(e) {
+      const data = e.currentTarget.dataset
+      const url = data.path
+      wx.switchTab({url})
+      this.setData({
+        selected: data.index
+      })
+    }
+  }
+})
